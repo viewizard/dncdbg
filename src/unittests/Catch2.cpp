e49c22cd9927d593297c5aded8f2ba993699e360
Mikhail Kurinnoi <viewizard@viewizard.com>
2026-01-24 13:27:54 +0300

    Remove unit tests.

--- src/unittests/Catch2.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020-2025 Samsung Electronics Co., Ltd.
-// Copyright (c) 2026 Mikhail Kurinnoi
-// See the LICENSE file in the project root for more information.
-
-#define CATCH_CONFIG_COLOUR_NONE
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
