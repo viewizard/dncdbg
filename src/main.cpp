27dd76710d50da8b51afe5d75929a8c418bfc166
Mikhail Kurinnoi <viewizard@viewizard.com>
2026-02-03 18:42:28 +0300

    Remove launch options run and attach.

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,7 @@
 #include <fcntl.h>
 #include <io.h>
 #include <windows.h>
-#define PATH_MAX MAX_PATH
 static void setenv(const char *var, const char *val, int) { _putenv_s(var, val); }
-#define getpid() (GetCurrentProcessId())
 #endif
 
 namespace dncdbg
@@ -30,12 +28,14 @@ namespace dncdbg
 
 static void print_help()
 {
-    fprintf(stdout, ".NET Core debugger\n"
+#ifdef _WIN32
+    fprintf(stdout, "Usage: dncdbg.exe [options]\n"
+#else
+    fprintf(stdout, "Usage: dncdbg [options]\n"
+#endif
                     "\n"
                     "Options:\n"
                     "--buildinfo                              Print build info.\n"
-                    "--attach <process-id>                    Attach the debugger to the specified process id.\n"
-                    "--run                                    Run program without waiting commands\n"
                     "--logProtocol=<path to log file>         Enable protocol interaction logging to file.\n"
                     "--log=<path to log file>                 Enable debugger logging to file.\n"
                     "                                         File log by default. File is created in 'current' folder.\n"
@@ -83,77 +83,18 @@ static void FindAndParseArgs(char **argv, std::vector<std::pair<std::string, std
     exit(EXIT_FAILURE);
 }
 
-static void CheckStartOptions(std::string &execFile, bool run)
-{
-    if (run && execFile.empty())
-    {
-        fprintf(stderr, "--run option was given, but no executable file specified!\n");
-        exit(EXIT_FAILURE);
-    }
-}
-
-static HRESULT AttachToExistingProcess(ManagedDebugger *pDebugger, DWORD pidDebuggee)
-{
-    HRESULT Status;
-    IfFailRet(pDebugger->Initialize());
-    IfFailRet(pDebugger->Attach(pidDebuggee));
-    return pDebugger->ConfigurationDone();
-}
-
-static HRESULT LaunchNewProcess(ManagedDebugger *pDebugger, std::string &execFile, std::vector<std::string> &execArgs)
-{
-    HRESULT Status;
-    IfFailRet(pDebugger->Initialize());
-
-    try
-    {
-        IfFailRet(pDebugger->Launch(execFile, execArgs, {}, {}, false));
-    }
-    catch (const std::exception &e)
-    {
-        fprintf(stderr, "%s\n", e.what());
-        exit(EXIT_FAILURE);
-    }
-
-    return pDebugger->ConfigurationDone();
-}
-
 int
 #if defined(_WIN32) && defined(_TARGET_X86_)
     __cdecl
 #endif
     main(int argc, char *argv[])
 {
-    DWORD pidDebuggee = 0;
     // prevent std::cout flush triggered by read operation on std::cin
     std::cin.tie(nullptr);
 
     std::string protocolLogFilePath;
 
-    std::string execFile;
-    std::vector<std::string> execArgs;
-
-    bool run = false;
-
     std::unordered_map<std::string, std::function<void(int &i)>> entireArguments{
-        {"--attach", [&](int &i) {
-            i++;
-            if (i >= argc)
-            {
-                fprintf(stderr, "Error: Missing process id\n");
-                exit(EXIT_FAILURE);
-            }
-            char *err;
-            pidDebuggee = strtoul(argv[i], &err, 10);
-            if (*err != 0)
-            {
-                fprintf(stderr, "Error: Missing process id\n");
-                exit(EXIT_FAILURE);
-            }
-        }},
-        {"--run", [&](int &i) {
-            run = true;
-        }},
         {"--help", [&](int &i) {
             print_help();
             exit(EXIT_SUCCESS);
@@ -168,22 +109,6 @@ int
         }},
         {"--interpreter=vscode" , [&](int &i) {
             // VSCode IDE send this option silently to debugger, just ignore it
-        }},
-        {"--", [&](int &i) {
-            ++i;
-            if (i < argc)
-            {
-                execFile = argv[i];
-            }
-            else
-            {
-                fprintf(stderr, "Error: Missing program argument\n");
-                exit(EXIT_FAILURE);
-            }
-            for (++i; i < argc; ++i)
-            {
-                execArgs.push_back(argv[i]);
-            }
         }}};
 
     std::vector<std::pair<std::string, std::function<void(int &i)>>> partialArguments{
@@ -208,8 +133,6 @@ int
         }
     }
 
-    CheckStartOptions(execFile, run);
-
     LOGI("DNCDbg started");
     // Note: there is no possibility to know which exception caused call to std::terminate
     std::set_terminate([] { LOGF("DNCDbg is terminated due to call to std::terminate: see stderr..."); });
@@ -219,17 +142,17 @@ int
     _setmode(_fileno(stdout), _O_BINARY);
 #endif
 
-    std::unique_ptr<DAP> protocol(new DAP(std::cin, std::cout));
+    DAP protocol(std::cin, std::cout);
 
     if (!protocolLogFilePath.empty())
     {
-        protocol->SetupProtocolLogging(protocolLogFilePath);
+        protocol.SetupProtocolLogging(protocolLogFilePath);
     }
 
     std::shared_ptr<ManagedDebugger> debugger;
     try
     {
-        debugger.reset(new ManagedDebugger(protocol.get()));
+        debugger.reset(new ManagedDebugger(&protocol));
     }
     catch (const std::exception &e)
     {
@@ -237,27 +160,8 @@ int
         exit(EXIT_FAILURE);
     }
 
-    protocol->SetDebugger(debugger);
-
-    if (!execFile.empty())
-        protocol->SetLaunchCommand(execFile, execArgs);
-
-    LOGI("pidDebugee %d", pidDebuggee);
-    HRESULT Status;
-    if (pidDebuggee != 0 && FAILED(Status = AttachToExistingProcess(debugger.get(), pidDebuggee)))
-    {
-        fprintf(stderr, "Error: 0x%08x failed to attach to %i\n", Status, pidDebuggee);
-        Interop::Shutdown();
-        return EXIT_FAILURE;
-    }
-    else if (run && FAILED(Status = LaunchNewProcess(debugger.get(), execFile, execArgs)))
-    {
-        fprintf(stderr, "Error: 0x%08x failed to launch new process\n", Status);
-        Interop::Shutdown();
-        return EXIT_FAILURE;
-    }
-
-    protocol->CommandLoop();
+    protocol.SetDebugger(debugger);
+    protocol.CommandLoop();
     Interop::Shutdown();
     return EXIT_SUCCESS;
 }
