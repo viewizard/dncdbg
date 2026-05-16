06d463c7a2db703d07c9806e8467bea0d036d7c0
Mikhail Kurinnoi <viewizard@viewizard.com>
2026-05-16 10:47:48 +0300

    Add more trace point tests

--- test-suite/TestTracePoint/Program.cs
+++ test-suite/TestTracePoint/Program.cs
@@ -25,7 +25,11 @@ class Program
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test5", null, null, "y={y}y}");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test6", null, null, "z={z}");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test7", null, null, "y={y{y}");
-                Context.AddBreakpoint(@"__FILE__:__LINE__", "bp1");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test8", null, null, "y=y}");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test9", null, null, "y={y");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test10", null, null, "i={i");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "bp1", null, "1");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "bp2");
                 Context.SetBreakpoints(@"__FILE__:__LINE__");
                 Context.PrepareEnd(@"__FILE__:__LINE__");
                 Context.WasEntryPointHit(@"__FILE__:__LINE__");
@@ -41,14 +45,32 @@ class Program
         x++;                                                         Label.Breakpoint("trace_test5");
         x++;                                                         Label.Breakpoint("trace_test6");
         x++;                                                         Label.Breakpoint("trace_test7");
+        x++;                                                         Label.Breakpoint("trace_test8");
+        x++;                                                         Label.Breakpoint("trace_test9");
 
-        Console.WriteLine("End");                                    Label.Breakpoint("bp1");
+        for (int i = 0; i < 2; i++)
+        {
+            x++;                                                     Label.Breakpoint("bp1");
+            x++;                                                     Label.Breakpoint("trace_test10");
+        }
+
+        Console.WriteLine("End");                                    Label.Breakpoint("bp2");
 
         Label.Checkpoint("trace_test", "finish",
             (Object context) =>
             {
                 Context Context = (Context)context;
                 Context.WasBreakpointHit(@"__FILE__:__LINE__", "bp1");
+
+                Context.RemoveBreakpoint(@"__FILE__:__LINE__", "bp1");
+                // change logMessate to already set bp
+                Context.RemoveBreakpoint(@"__FILE__:__LINE__", "trace_test10");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "trace_test10", null, null, "i={i}");
+                Context.SetBreakpoints(@"__FILE__:__LINE__");
+
+                Context.Continue(@"__FILE__:__LINE__");
+
+                Context.WasBreakpointHit(@"__FILE__:__LINE__", "bp2");
 
                 Context.WasOutputEvent("console", "Start test.\n", @"__FILE__:__LINE__");
                 Context.WasOutputEvent("console", "x=1\n", @"__FILE__:__LINE__");
@@ -57,6 +79,11 @@ class Program
                 Context.WasOutputEvent("console", "y=3y}\n", @"__FILE__:__LINE__");
                 Context.WasOutputEvent("console", "z={error: The name 'z' does not exist in the current context}\n", @"__FILE__:__LINE__");
                 Context.WasOutputEvent("console", "y={y{y}\n", @"__FILE__:__LINE__");
+                Context.WasOutputEvent("console", "y=y}\n", @"__FILE__:__LINE__");
+                Context.WasOutputEvent("console", "y={y\n", @"__FILE__:__LINE__");
+
+                Context.WasOutputEvent("console", "i={i\n", @"__FILE__:__LINE__");
+                Context.WasOutputEvent("console", "i=1\n", @"__FILE__:__LINE__");
 
                 Context.Continue(@"__FILE__:__LINE__");
             });
