4e3dbf4065df10f76aab747f74e00ef5504eead1
Mikhail Kurinnoi <viewizard@viewizard.com>
2026-08-06 21:08:52 +0300

    Add more TestMethodParameters tests

--- test-suite/TestMethodParameters/Program.cs
+++ test-suite/TestMethodParameters/Program.cs
@@ -24,7 +24,7 @@ class TestClass
 
     public async Task<int> FetchDataAsync(int i1, string s1, TestGeneric<int, string> u1)
     {
-        await Task.Delay(1000);
+        await Task.Delay(100);
         return 5;                                                               Label.Breakpoint("instance3");
     }
 }
@@ -36,9 +36,21 @@ public class GenClass<X, Y>
         ;                                                                      Label.Breakpoint("instance4");
     }
 
+    public async Task<int> FetchDataAsync<W>(X i1, Y s1, W u1)
+    {
+        await Task.Delay(100);
+        return 5;                                                               Label.Breakpoint("instance6");
+    }
+
     public static void Cde<W>(X s2, Y i2, W u2)
     {
         ;                                                                      Label.Breakpoint("static4");
+    }
+
+    public static async Task<int> StaticFetchDataAsync<W>(X s2, Y i2, W u2)
+    {
+        await Task.Delay(100);
+        return 5;                                                               Label.Breakpoint("static6");
     }
 }
 
@@ -51,7 +63,7 @@ class Program
 
     static async Task<int> StaticFetchDataAsync(string s2, int i2, TestGeneric<int, string> u2)
     {
-        await Task.Delay(1000);
+        await Task.Delay(100);
         return 5;                                                               Label.Breakpoint("static3");
     }
 
@@ -66,9 +78,11 @@ class Program
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "static1");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "static3");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "static4");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "static6");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "instance1");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "instance3");
                 Context.AddBreakpoint(@"__FILE__:__LINE__", "instance4");
+                Context.AddBreakpoint(@"__FILE__:__LINE__", "instance6");
                 Context.SetBreakpoints(@"__FILE__:__LINE__");
                 Context.ConfigurationDone(@"__FILE__:__LINE__");
 
@@ -81,6 +95,7 @@ class Program
         Bcd("test", 1, genParam);                                                   Label.Breakpoint("static2");
         int i = StaticFetchDataAsync("test", 1, genParam).GetAwaiter().GetResult();
         GenClass<string, int>.Cde<TestGeneric<int, string>>("test", 1, genParam);   Label.Breakpoint("static5");
+        i = GenClass<string, int>.StaticFetchDataAsync<TestGeneric<int, string>>("test", 1, genParam).GetAwaiter().GetResult();
 
         Label.Checkpoint("test_static", "test_instance",
             (Object context) =>
@@ -100,6 +115,11 @@ class Program
                 string[] stacktrace3 = { "static4", "static5" };
                 Context.TestStackTrace(@"__FILE__:__LINE__", "TestMethodParameters.GenClass<string, int>.Cde<TestMethodParameters.TestGeneric<int, string>>(string s2, int i2, TestMethodParameters.TestGeneric<int, string> u2)", stacktrace3, 2);
                 Context.Continue(@"__FILE__:__LINE__");
+
+                Context.WasBreakpointHit(@"__FILE__:__LINE__", "static6");
+                string[] stacktrace4 = { "static6" };
+                Context.TestStackTrace(@"__FILE__:__LINE__", "TestMethodParameters.GenClass<string, int>.StaticFetchDataAsync<TestMethodParameters.TestGeneric<int, string>>(string s2, int i2, TestMethodParameters.TestGeneric<int, string> u2)", stacktrace4, 1);
+                Context.Continue(@"__FILE__:__LINE__");
             });
 
         // test instance
@@ -108,6 +128,7 @@ class Program
         i = testClass.FetchDataAsync(1, "test", genParam).GetAwaiter().GetResult();
         GenClass<int, string> genClass = new GenClass<int, string>();
         genClass.Ecd<TestGeneric<int, string>>(1, "test", genParam);                Label.Breakpoint("instance5");
+        i = genClass.FetchDataAsync<TestGeneric<int, string>>(1, "test", genParam).GetAwaiter().GetResult();
 
         Label.Checkpoint("test_instance", "finish",
             (Object context) =>
@@ -126,6 +147,11 @@ class Program
                 Context.WasBreakpointHit(@"__FILE__:__LINE__", "instance4");
                 string[] stacktrace3 = { "instance4", "instance5" };
                 Context.TestStackTrace(@"__FILE__:__LINE__", "TestMethodParameters.GenClass<int, string>.Ecd<TestMethodParameters.TestGeneric<int, string>>(int i1, string s1, TestMethodParameters.TestGeneric<int, string> u1)", stacktrace3, 2);
+                Context.Continue(@"__FILE__:__LINE__");
+
+                Context.WasBreakpointHit(@"__FILE__:__LINE__", "instance6");
+                string[] stacktrace4 = { "instance6" };
+                Context.TestStackTrace(@"__FILE__:__LINE__", "TestMethodParameters.GenClass<int, string>.FetchDataAsync<TestMethodParameters.TestGeneric<int, string>>(int i1, string s1, TestMethodParameters.TestGeneric<int, string> u1)", stacktrace4, 1);
                 Context.Continue(@"__FILE__:__LINE__");
             });
 
