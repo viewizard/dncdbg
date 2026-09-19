4dc6aae569951f719f489b0a2f267ef710537c61
Mikhail Kurinnoi <viewizard@viewizard.com>
2026-09-19 16:17:40 +0300

    Add TestSourceLink

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -43,6 +43,7 @@ Upcoming changes compared to previous version.
 - Added resuming a runtime suspended on its default diagnostics endpoint during attach.
 - Added TestAttachToSuspend.
 - Added [Source Link](https://github.com/dotnet/sourcelink/blob/main/README.md) support.
+- Added TestSourceLink.
 
 #### Changed
 - Updated tree-sitter version to 0.27.0.
---
+++ test-suite/TestSourceLink/Program.cs
@@ -0,0 +1,58 @@
+using System;
+
+using DbgTest;
+using DbgTest.DAP;
+using DbgTest.Script;
+
+namespace TestSourceLink
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        Label.Checkpoint("init", "sources_test",
+            (Object context) =>
+            {
+                Context Context = (Context)context;
+                Context.Initialize(@"__FILE__:__LINE__");
+                Context.Launch(JMC: null, StepFiltering: null, RemoteConsole: false, RemoteConsolePort: 0, @"__FILE__:__LINE__");
+                Context.AddBreakpointAndAddID(@"__FILE__:__LINE__", "bp1");
+                Context.SetBreakpointsAndCheckIDs(@"__FILE__:__LINE__");
+                Context.ConfigurationDone(@"__FILE__:__LINE__");
+
+                Context.WasEntryPointHit(@"__FILE__:__LINE__");
+                Context.Continue(@"__FILE__:__LINE__");
+            });
+
+        ;                                                                 Label.Breakpoint("bp1");
+
+        Label.Checkpoint("sources_test", "finish",
+            (Object context) =>
+            {
+                Context Context = (Context)context;
+                Context.WasBreakpointHit(@"__FILE__:__LINE__", "bp1", CheckSourcePath: false);
+
+                int sourceReference = Context.GetSourceReferenceFromTopFrame(@"__FILE__:__LINE__");
+                Context.CompareSources(@"__FILE__:__LINE__", "Program.cs", SourceReference: sourceReference, SourcePath: "");
+
+                Context.CompareSources(@"__FILE__:__LINE__", "Program2.cs", SourceReference: 0, SourcePath: "Program2.cs");
+
+                sourceReference = Context.GetSourceReferenceFromLoadedSources(@"__FILE__:__LINE__", SourceName: "Program.cs");
+                Context.CompareSources(@"__FILE__:__LINE__", "Program.cs", SourceReference: sourceReference, SourcePath: "");
+
+                sourceReference = Context.GetSourceReferenceFromLoadedSources(@"__FILE__:__LINE__", SourceName: "Program2.cs");
+                Context.CompareSources(@"__FILE__:__LINE__", "Program2.cs", SourceReference: sourceReference, SourcePath: "");
+
+                Context.Continue(@"__FILE__:__LINE__");
+            });
+
+        Label.Checkpoint("finish", "",
+            (Object context) =>
+            {
+                Context Context = (Context)context;
+                Context.WasExit(0, @"__FILE__:__LINE__");
+                Context.DebuggerExit(@"__FILE__:__LINE__");
+            });
+    }
+}
+}
